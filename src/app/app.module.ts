e635259396b0eb95afdfcf539adb022d53d48b3f
salah <salah.yousef.muhammad@gmail.com>
2019-04-10 23:47:54 +0200

    create component services and install ng material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MaterialModule } from "./shared/material.module";
+
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { ManufacturersComponent } from './components/manufacturers/manufacturers.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BrandsComponent,
+    ManufacturersComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
